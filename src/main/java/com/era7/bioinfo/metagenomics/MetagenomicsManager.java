53dfc4c5ec65c42d22d89ab264ebfb1db1c4e8b1
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2012-02-13 12:41:58 +0100

    some updates

--- src/main/java/com/era7/bioinfo/metagenomics/MetagenomicsManager.java
+++ src/main/java/com/era7/bioinfo/metagenomics/MetagenomicsManager.java
@@ -24,7 +24,6 @@ public class MetagenomicsManager extends Bio4jManager{
     
     private static String PROVIDER_ST = "provider";
     private static String EXACT_ST = "exact";
-    //private static String FULL_TEXT_ST = "fulltext";
     private static String LUCENE_ST = "lucene";
     private static String TYPE_ST = "type";
     
